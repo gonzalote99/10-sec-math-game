612714b5657b2f80e4f9614eea4a8a1169089833
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-11-17 13:10:26 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 10-sec-math-game
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/10-sec-math-game)
---
+++ script.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const MAX_QUESTIONS = 10;
+  const COUNTDOWN_SECONDS = 10;
+  let currentQuestion;
+  let interval;
+  let timeLeft = COUNTDOWN_SECONDS;
+   let score = 0;
+   let questionRemaining = MAX_QUESTIONS;
+
+   const scoreElement = document.querySelector('#score');
+   const userInput = document.querySelector('#user-input');
+   const restartButton = document.querySelector('#restart-button');
+   const equationElement = document.querySelector('.equation');
+
+   const updateTimeLeft = (amount) => {
+     timeLeft += amount;
+   }
+
+   const updateScore = (amount) => {
+     score += amount;
+     scoreElement.textContent = score;
+   };
+
+   const startGame = () => {
+     if(!interval) {
+       interval = setInterval(() => {
+         updateTimeLeft(-1);
+         if(timeLeft <= 0 || questionRemaining <= 0) {
+           clearInterval(interval);
+           endGame();
+         }
+       }, 1000);
+       renderNewQuestion();
+     }
+   };
+
+   const randomNumberGenerator = (size) => {
+     return Math.ceil(Math.random() * size);
+   };
+
+   const questionGenerator = () => {
+     const num1 = randomNumberGenerator(10);
+     const num2 = randomNumberGenerator(10);
+     return {
+       answer: num1 + num2,
+       equation: `${num1} + ${num2}`,
+
+     };
+   };
+
+   const renderNewQuestion = () => {
+    currentQuestion = questionGenerator();
+    equationElement.textContent = currentQuestion.equation;
+   };
+
+   const checkAnswer = () => {
+     if(Number(userInput.value) === currentQuestion.answer) {
+       renderNewQuestion();
+       userInput.value = '';
+       updateTimeLeft(1);
+       updateScore(1);
+       questionRemaining--;
+     }
+   };
+
+   const endGame = () => {
+      userInput.disabled = true;
+      restartButton.disabled = false;
+   }
+
+   userInput.addEventListener('keydown', function (event)  {
+   if(event.key === 'Enter') {
+     startGame();
+     checkAnswer();
+   }
+   });
+
+   restartButton.addEventListener('click', function() {
+     clearInterval();
+     interval = null;
+     timeLeft = COUNTDOWN_SECONDS;
+     score = 0;
+     questionRemaining = MAX_QUESTIONS;
+     scoreElement.textContent = score;
+     userInput.disabled = false;
+     restartButton.disabled = true;
+     startGame();
+   })
+
+   startGame();
+})
